f17e10bcaed96a103402b30b75c0527d8e3359e1
Santhosh T <SANTHOSH-TNJ@lntecc.com>
2021-07-12 12:24:00 +0530

    test

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { LandingComponent } from './landing/landing.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: "", component: LandingComponent, pathMatch: "full" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
